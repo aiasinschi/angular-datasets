9cbd7e34fd3463113105b481bd9493f2030d27b4
Adrian Iasinschi <aiasinschi@gmail.com>
2017-10-14 11:03:47 +0300

    Added mongodb connection and initialization stuff

--- server.js
+++ server.js
@@ -1,15 +1,56 @@
 var http = require('http');
 var fs = require("fs");
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+  }
+}
+var db = null,
+    dbDetails = new Object();
+
+var initDb = function(callback) {
+  if (mongoURL == null) return;
+
+  var mongodb = require('mongodb');
+  if (mongodb == null) return;
+
+  mongodb.connect(mongoURL, function(err, conn) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    db = conn;
+    dbDetails.databaseName = db.databaseName;
+    dbDetails.url = mongoURLLabel;
+    dbDetails.type = 'MongoDB';
+
+    console.log('Connected to MongoDB at: %s', mongoURL);
+  });
+};
 
 http.createServer(function(request, response) {
-    if(request.url === "/index"){
+    if ((request.url === "/index") || (request.url === "/")){
 		sendFileContent(response, "index.html", "text/html");
-	}
-	else if(request.url === "/"){
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.write('<b>Hey there!</b><br /><br />This is the default response. Requested URL is: ' + request.url);
 	}
 	else if(/^\/[a-zA-Z0-9\/]*.js$/.test(request.url.toString())){
 		sendFileContent(response, request.url.toString().substring(1), "text/javascript");
@@ -17,10 +58,13 @@ http.createServer(function(request, response) {
 	else if(/^\/[a-zA-Z0-9\/]*.css$/.test(request.url.toString())){
 		sendFileContent(response, request.url.toString().substring(1), "text/css");
 	}
-	else{
+	else {
 		console.log("Requested URL is: " + request.url);
 		response.end();
 	}
+    initDb(function(err){
+        console.log('Error connecting to Mongo. Message:\n'+err);
+    });
 }).listen(port, ip);
 
 function sendFileContent(response, fileName, contentType){
